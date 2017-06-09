456c786081c34ae26cef3e216e88382e899aed18
Justin Wong <justinlikefish@qq.com>
2017-06-09 21:01:28 +0800

    安卓默认信任所有HTTPS站点以防止打不开的问题

--- quick/templates/lua-template-quick/frameworks/runtime-src/proj.android/src/org/cocos2dx/lib/QuickHTTPInterface.java
+++ quick/templates/lua-template-quick/frameworks/runtime-src/proj.android/src/org/cocos2dx/lib/QuickHTTPInterface.java
@@ -9,6 +9,8 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -16,6 +18,13 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,7 +41,15 @@ public class QuickHTTPInterface {
         HttpURLConnection urlConnection;
         try {
             url = new URL(strURL);
-            urlConnection = (HttpURLConnection)url.openConnection();
+    		//ignore https certificate validation |忽略 https 证书验证
+    		if (url.getProtocol().toUpperCase().equals("HTTPS")) {
+    			trustAllHosts();
+    			HttpsURLConnection https = (HttpsURLConnection) url.openConnection();
+    			https.setHostnameVerifier(DO_NOT_VERIFY);
+    			urlConnection = https;
+    		} else {
+    			urlConnection = (HttpURLConnection)url.openConnection();
+    		}
             urlConnection.setRequestProperty("Accept-Encoding", "identity");
             urlConnection.setDoInput(true);
         } catch (Exception e) {
@@ -450,4 +467,37 @@ public class QuickHTTPInterface {
         return Long.toString(millisSecond);
     }
 
+    public static void trustAllHosts() {
+    	// Create a trust manager that does not validate certificate chains
+    	// Android use X509 cert
+    	TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+    		public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+    			return new java.security.cert.X509Certificate[] {};
+    		}
+
+    		public void checkClientTrusted(X509Certificate[] chain,
+    				String authType) throws CertificateException {
+    		}
+
+    		public void checkServerTrusted(X509Certificate[] chain,
+    				String authType) throws CertificateException {
+    		}
+    	} };
+
+    	// Install the all-trusting trust manager
+    	try {
+    		SSLContext sc = SSLContext.getInstance("TLS");
+    		sc.init(null, trustAllCerts, new java.security.SecureRandom());
+    		HttpsURLConnection
+    				.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    }
+
+    public final static HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+    	public boolean verify(String hostname, SSLSession session) {
+    		return true;
+    	}
+    };
 }
